4f0a979a0bda9ce4f0457119da3b6b80852b2b55
SaidaDev <saida_musaxonova@student.itpu.uz>
2026-04-18 13:31:17 +0500

    feat: do some exercises

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+"use strict";
 let message = "Hello,World";
 let heading = document.createElement("h1");
 heading.textContent = message;
-document.body.appendChild(heading);
-
+document.appendChild(heading);
