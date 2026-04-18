85ef3ee3b64ed687da0261c8bc79ec877f6dafa1
SaidaDev <saida_musaxonova@student.itpu.uz>
2026-04-18 08:48:16 +0500

    feat: do some ts

---
+++ variableMe.js
@@ -0,0 +1,3 @@
+"use strict";
+let greeting = "Hello Saida";
+console.log(greeting);
---
+++ variableMe.ts
@@ -0,0 +1,14 @@
+let greeting : string = "Hello Saida"
+console.log(greeting);
+
+greeting.toLowerCase()
+ 
+// number
+ 
+let userId: number = 334455.3 
+
+// boolean
+
+let isLoggedIn : boolean = false
+
+export{} 
